22c9738adf84d7bbbebe223458b902f0d143026a
Axeer <vip.slepkov@gmail.com>
2021-12-12 01:38:02 +0300

    helicopter helicopter helicopter

---
+++ interpreter.py
@@ -0,0 +1,159 @@
+import sys
+import time
+
+stop = False
+running = False
+
+max_time_secs = 5 # funnyword alert!
+
+
+class Message:
+    infinityloop = 'falling into infinity loop'
+    nonepath = 'please enter a path to bf file'
+
+
+class Keywords:
+    keywords = '-+><[],.#'
+    debug = '#'
+    comment = '!' #TODO
+
+class Loop:
+    enter_ptr = None
+    start_pos = None
+    end_pos = None
+    #TODO if on end loop enter_ptr == outer_ptr: eto infinity loop
+
+def exit():
+    running = False
+    sys.exit()
+
+
+def execute(file):
+    global stop
+    global running
+    global ignore
+    stop = False
+    running = True
+    ignore = False
+
+    with open(file, encoding='ascii') as program:
+        program_content = program.read()
+        program_content = [ i for i in program_content if i in Keywords.keywords]
+
+    position = 0
+    pointer = 0
+    memory = [0]
+    loop_enter_ptr = 0
+    loop_enter_val = 0
+
+    bracemap = build_bracemap(program_content)
+
+    class Debug:
+        @staticmethod
+        def out():
+            curmem = 0
+            print("\n|{0:4}| -> |{1:4}|".format("memcell:", "contains:"))
+            for memcell in memory:
+                print(f"{curmem:5} {'->'} {memcell:5}")
+                curmem += 1
+            print(f"cur ptr: {pointer} (value: {memory[pointer]})")
+            print(f"cur pos: {position}(instruction {program_content[position-1]})")
+            input("enter any key to continue executing...")
+    time_start = time.time()
+
+    def match(sym):
+        return program_content[position] == sym
+
+    def out():
+        print(chr(memory[pointer]), end="")
+
+    while position < len(program_content):
+        if match(">"):
+            pointer += 1
+            if len(memory) <= pointer:
+                memory.append(0)
+                
+        elif match("<"):
+            pointer -= 1
+            if pointer < 0:
+                print("Range error")
+                exit()
+
+        elif match("+"):
+            memory[pointer] += 1
+            if memory[pointer] > 255:
+                memory[pointer] = 0
+                
+        elif match("-"):
+            memory[pointer] -= 1
+            if memory[pointer] < 0:
+                memory[pointer] = 255
+                
+        elif match("."):
+            out()
+            
+        elif match(","):
+            memory[pointer] = ord(input()[0])
+
+        elif match("[") and memory[pointer] == 0:
+            try:
+                position = bracemap[position]
+            except KeyError:
+                print("Key Error")
+                exit()
+
+        elif match("[") and memory[pointer] != 0:
+            # FIXME
+            loop_enter_val = memory[pointer]
+            loop_enter_ptr = pointer
+
+        elif match("]") and memory[pointer] != 0:
+            try:
+                if loop_enter_val <= memory[loop_enter_ptr]:
+                    print(Message.infinityloop)
+                    exit()
+                position = bracemap[position]
+            except KeyError:
+                print("Key Error")
+                exit()
+
+        elif match(Keywords.debug):
+            Debug.out()
+
+        if stop:
+            exit()
+        
+        position += 1
+        time_end = time.time()
+
+        if (time_end - time_start) > max_time_secs:
+            pass
+
+    running = False
+    exit()
+
+
+def build_bracemap(code):
+    temp = []
+    bracemap = {}
+
+    try:
+        for position, command in enumerate(code):
+            if command == "[": temp.append(position)
+            if command == "]":
+                start = temp.pop()
+                bracemap[start] = position
+                bracemap[position] = start
+        return bracemap
+    except IndexError:
+        print("Parse Error")
+        exit()
+
+if __name__ == '__main__':
+    try:
+        bffile = sys.argv[1]
+    except:
+        print(Message.nonepath)
+        exit()
+    if bffile is not None:
+        execute(bffile)
